969a16a63c57918b76cca3517e4fdba8e575c752
TiestoTool <9019545@student.zadkine.nl>
2024-09-13 11:38:36 +0200

    1.2

--- index.php
+++ index.php
@@ -10,3 +10,32 @@ echo "Leeftijd: " . $age . "<br>";
 echo "Lengte: " . $height . " meter<br>";
 echo "Is student: " . ($isStudent ? 'Ja' : 'Nee') . "<br>";
 ?>
+
+<!-- 
+///correcte variablenamen:    
+
+    $firstName
+    $age
+    $user_height
+    $_isActive
+    $totalAmount
+
+Vijf ongeldige variabelenamen:
+1name
+Variabelenamen mogen niet met een cijfer beginnen.
+
+
+@age
+Ongeldig: Variabelenamen mogen geen speciale tekens bevatten, behalve een underscore (_).
+Correctie: $age
+
+user-name
+Ongeldig: Het gebruik van het minteken (-) is niet toegestaan.
+
+
+total amount
+Ongeldig: Variabelenamen mogen geen spaties bevatten.
+
+
+class
+Ongeldig: class is een gereserveerd woord in PHP en kan niet als variabelenaam worden gebruikt.
