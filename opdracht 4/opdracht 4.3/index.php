00686633169bdedb3b3f3fd63e508298dfd544ef
TiestoTool <9019545@student.zadkine.nl>
2024-09-13 14:14:46 +0200

    opdracht 4.3

---
+++ opdracht 4/opdracht 4.3/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$pets = [
+    ["species" => "Hond", "name" => "Buddy", "age" => 3],
+    ["species" => "Papegaai", "name" => "Coco", "age" => 6],
+    ["species" => "Konijn", "name" => "Nibbles", "age" => 2]
+];
+
+echo $pets[2]["name"] . " is een " . $pets[2]["species"] . "<br>";
+
+$pets[1]["age"] += 1;
+
+$pets[] = ["species" => "Kat", "name" => "Simba", "age" => 2];
+
+print_r($pets);
+?>
