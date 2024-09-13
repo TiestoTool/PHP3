61e6c9cb845fd322475a57847b1400a93a24b9c8
TiestoTool <9019545@student.zadkine.nl>
2024-09-13 14:10:54 +0200

    opdracht 4.1

---
+++ opdracht 4/opdracht 4.1/index.php
@@ -0,0 +1,16 @@
+<?php
+$products = [
+    ["Laptop", 999.99, 30],
+    ["Smartphone", 699.99, 50],
+    ["Monitor", 199.99, 75],
+    ["Toetsenbord", 49.99, 100]
+];
+
+echo $products[1][0] . " kost " . $products[1][1] . " euro<br>";
+
+$products[3][2] = 120;
+
+$products[] = ["Tablet", 299.99, 50];
+
+print_r($products);
+?>
