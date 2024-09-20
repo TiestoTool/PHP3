514757a98764198a9ac4017c0a4f39b3977410ef
TiestoTool <9019545@student.zadkine.nl>
2024-09-20 13:40:57 +0200

    5.4

---
+++ opdracht 5/opdracht 5.4/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$countries = ['Nederland ', 'Belgie ', 'Duitsland '];
+$city = ['Amsterdam', 'Brussel', 'Berlijn'];
+
+echo "De hoofdtad van ". $countries[0]. "is ". $city[0];  
+
+echo "De hoofdtad van ". $countries[1]. "is ". $city[1];
+
+echo "De hoofdtad van ". $countries[2]. "is ". $city[2];
+
+?>
